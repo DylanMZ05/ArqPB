fa79e66c1b0857cadedfa88534e621348f0cd5a6
Dylan Mazur Zgainer <136861567+DylanMZ05@users.noreply.github.com>
2024-10-07 23:59:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     menu.classList.toggle('active');
 });
+
+document.addEventListener('scroll', function() {
+    const scrollPosition = window.scrollY; // Obtiene la posición actual del scroll
+    const firstSection = document.querySelector('.first-section');
+    
+    // Ajusta la posición del background en función del scroll, dividiendo por 4 para un desplazamiento lento
+    firstSection.style.backgroundPositionY = `${scrollPosition * 0.5}px`;
+});
